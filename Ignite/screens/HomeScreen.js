d9b24356605d4049702b602eaf2cfaf814d19ca6
Chanheeshin <chanheeshin@live.com>
2019-04-26 21:57:10 -0600

    Modified HomeScreen.js
    - Looks closer to the design Katie drew up earlier. But still requires A LOT of work.
    -Lot's of color coding so we can, while developing, see what the different Views actually cover.
    - Will need to be cleaned up and have functions added.

--- Ignite/screens/HomeScreen.js
+++ Ignite/screens/HomeScreen.js
@@ -7,6 +7,7 @@ import {
   Text,
   TouchableOpacity,
   View,
+  TextInput,
 } from 'react-native';
 
 export default class HomeScreen extends React.Component {
@@ -18,46 +19,92 @@ export default class HomeScreen extends React.Component {
     return (
       <View style={styles.container}>
 
-        <View style={styles.ProjectTitle}>
-          <Text style={styles.ProjectTitleText}>
-            [Project Name]
-          </Text>
-        </View>
-
         <View style={styles.SignInBox}>
-          <Text style={styles.ProjectTitleText}>
-            gerdgsdrg
-          </Text>
+
+          <View style={styles.ProjectTitleBox}>
+            <Text style={styles.ProjectTitleText}>
+              [Project Name]
+            </Text>
+          </View>
+
+          <View style={styles.usernameBox}>
+            <TextInput
+                style={styles.usernameInput}
+                placeholder="Username..."
+            />
+          </View>
+
+          <View style={styles.passwordBox}>
+            <TextInput
+                style={styles.passwordInput}
+                placeholder="Password..."
+            />
+          </View>
+
 
         </View>
+
+        <View style={styles.SignUpBox}>
+
+        </View>
+
+
+
       </View>
     );
   }}
 
 const styles = StyleSheet.create({
-  ProjectTitleText:{
-    fontSize: 20,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  ProjectTitle: {
-    paddingTop: 40,
-    marginBottom: 10,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    alignItems: 'center',
-    backgroundColor: '#a9a9a9',
-    paddingVertical: 15,
-  },
   SignInBox: {
     alignItems: 'center',
-    marginTop: 50,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: '#ffffff'
+    backgroundColor: '#a9a9a9',
+    flex: 0.65
+  },
+  ProjectTitleBox:{
+    flex: 0.6,
+//    backgroundColor: '#a9a229',
+    alignItems: 'center',
+    width: "100%"
+  },
+  ProjectTitleText:{
+    fontSize: 40,
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    flex: 1
+  },
+  usernameBox:{
+    flex: 0.2,
+    alignItems: 'center',
+    justifyContent: 'center',
+//    backgroundColor: '#04a900',
+    width: '100%'
+  },
+  usernameInput:{
+    height: 40,
+    width: '70%',
+    borderColor: 'grey',
+    borderWidth: 1,
+    paddingLeft: 6,
+    fontSize: 20
+  },
+  passwordBox:{
+    flex: 0.2,
+    alignItems: 'center',
+    justifyContent: 'center',
+//    backgroundColor: '#a90700',
+    width: '100%'
+  },
+  passwordInput:{
+    height: 40,
+    width: '70%',
+    borderColor: 'grey',
+    borderWidth: 1,
+    paddingLeft: 6,
+    fontSize: 20
+  },
+  SignUpBox: {
+    flex: 0.35,
+    backgroundColor: '#4aa99e',
   },
   container: {
     flex: 1,
